ee3ab93623e31fc977e2bf387a53f476ef50ffc6
Raimo Tuisku <raimo.tuisku@gmail.com>
2015-06-08 00:25:16 -0700

    Update locations

--- Assets/Morue/MorueBehavior.cs
+++ Assets/Morue/MorueBehavior.cs
@@ -5,22 +5,22 @@ using System.Collections;
 public class MorueBehavior : MonoBehaviour {
 	public GameObject morue;
 	float delta;
-	float angle=1;
+	float angle=3;
 	float angleDelta=0.1f;
 	// Use this for initialization
 	void Start () {
-		delta = Time.deltaTime * 140f;
+		morue = gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		morue.transform.Translate (delta, 0, 0);
-//		morue.transform.RotateAround (morue.transform.position, new Vector3 (0,0,1), angle);
-		if (Math.Abs(morue.transform.position.x) > 150) {
+		morue.transform.Translate (Time.deltaTime * 40f, 0, 0);
+		morue.transform.RotateAround (morue.transform.position, new Vector3 (0,1,0), angle);
+		if (Math.Abs(morue.transform.position.x) > 250 || Math.Abs(morue.transform.position.z) > 250) {
 			morue.transform.RotateAround (morue.transform.position, new Vector3 (0,1,0), 180);
 		}
 		angle += angleDelta;
-		if (Math.Abs (angle) > 1f)
+		if (Math.Abs (angle) > 3f)
 			angleDelta = - angleDelta;
 	}
 }
