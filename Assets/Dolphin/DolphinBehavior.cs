61df2062ee68feb325f49894464bba0620146464
Raimo Tuisku <raimo.tuisku@gmail.com>
2015-06-07 21:21:34 -0700

    Update DolphinBehavior

--- Assets/Dolphin/DolphinBehavior.cs
+++ Assets/Dolphin/DolphinBehavior.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System;
 
 public class DolphinBehavior : MonoBehaviour {
-	public GameObject dolphin;
 	float delta;
 	float angle=1;
 	float angleDelta=-0.1f;
@@ -11,18 +10,18 @@ public class DolphinBehavior : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		delta = -Time.deltaTime * 140f;
-		dolphin.transform.RotateAround (dolphin.transform.position, new Vector3 (0,0,1), angle/angleDelta);
+		gameObject.transform.RotateAround (gameObject.transform.position, new Vector3 (0,0,1), angle/angleDelta);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		dolphin.transform.Translate (delta, 0, 0);
-		dolphin.transform.RotateAround (dolphin.transform.position, new Vector3 (0,0,1), angle);
-		if (Math.Abs(dolphin.transform.position.x) > 150) {
-			dolphin.transform.RotateAround (dolphin.transform.position, new Vector3 (0,1,0), 180);
+		gameObject.transform.Translate (delta, 0, 0);
+		gameObject.transform.RotateAround (gameObject.transform.position, new Vector3 (0,0,1), angle);
+		if (Math.Abs(gameObject.transform.position.x) > 150) {
+			gameObject.transform.RotateAround (gameObject.transform.position, new Vector3 (0,1,0), 180);
 		}
-		if (Math.Abs(dolphin.transform.position.y) > 100) {
-			dolphin.transform.RotateAround (dolphin.transform.position, new Vector3 (0,0,1), 180);
+		if (Math.Abs(gameObject.transform.position.y) > 0) {
+			gameObject.transform.RotateAround (gameObject.transform.position, new Vector3 (0,0,1), 180);
 		}
 		angle += angleDelta;
 		if (Math.Abs (angle) > 1f)
